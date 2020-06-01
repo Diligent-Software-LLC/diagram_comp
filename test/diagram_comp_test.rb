426abe2db7427695dd9c5fc4823189b8d13687c4
Bradley J. Tannor <bradleytannor@gmail.com>
2020-06-01 14:49:31 -0700

    Development Complete
    Added test coverage.

--- test/diagram_comp_test.rb
+++ test/diagram_comp_test.rb
@@ -5,6 +5,9 @@ require_relative 'test_helper'
 #   Tests the DiagramComposition class.
 class DiagramCompositionTest < Minitest::Test
 
+  # Constants.
+  CLASS = Diagram
+
   # test_conf_doc_f_ex().
   # @description
   #   The .travis.yml, CODE_OF_CONDUCT.md, Gemfile, LICENSE.txt, README.md,
@@ -39,7 +42,66 @@ class DiagramCompositionTest < Minitest::Test
   #   Set fixtures.
   def setup()
   end
-  
+
+  # Diagram.diagrammable().
+
+  # test_diagrammable_x().
+  # @description
+  #   DIAGRAMMABLE.
+  def test_diagrammable_x()
+    expected = Set[Node, LinkedList]
+    assert_equal(expected, CLASS.diagrammable())
+  end
+
+  # initialize().
+
+  # test_no_init().
+  # @description
+  #   Diagram's 'iniitialize' is unresponsive.
+  def test_no_init()
+    refute_respond_to(CLASS, :initialize)
+  end
+
+  # Diagram.backward_arrow().
+
+  # test_cba_x().
+  # @description
+  #   BACKWARD_ARROW.
+  def test_cba_x()
+    expected = '<--'.freeze()
+    assert_equal(expected, CLASS.backward_arrow())
+  end
+
+  # Diagram.forward_arrow().
+
+  # test_cfa_x().
+  # @description
+  #   FORWARD_ARROW.
+  def test_cfa_x()
+    expected = '-->'.freeze()
+    assert_equal(expected, Diagram.forward_arrow())
+  end
+
+  # Diagram.pipe().
+
+  # test_cp_x().
+  # @description
+  #   PIPE.
+  def test_cp_x()
+    expected = '|'.freeze()
+    assert_equal(expected, CLASS.pipe())
+  end
+
+  # Diagram.space().
+
+  # test_cs_x().
+  # @description
+  #   SPACE.
+  def test_cs_x()
+    expected = ' '.freeze()
+    assert_equal(expected, CLASS.space())
+  end
+
   # teardown().
   # @description
   #   Cleanup.
