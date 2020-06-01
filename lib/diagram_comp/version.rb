960d553cd1658cd6b3c855e56011efed6695393d
Bradley J. Tannor <bradleytannor@gmail.com>
2020-06-01 13:21:49 -0700

    Setup Complete
    Bumped Major Version. Deleted linked_list_library and node dependencies. Added linked_list_comp, node_comp, data_library, node_error, and diagram_factory_comp development dependencies. Bumped diagram_impl.

--- lib/diagram_comp/version.rb
+++ lib/diagram_comp/version.rb
@@ -5,5 +5,5 @@
 # @class_description
 #   A Diagram composition. Composites the Interface and Implementation.
 class DiagramComposition
-  VERSION = '0.1.0'.freeze()
+  VERSION = '1.0.0'.freeze()
 end
